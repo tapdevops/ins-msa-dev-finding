4a9a03f31d23ce74171b834ee7d841a589e75b77
nicholas <silvranz@gmail.com>
2019-07-31 09:27:39 +0700

    command buat install kafka-node : npm install kafka-node --no-optional --save Contoh producer: server.js Contoh consumer: test.js Butuh kafka(broker) & zookeeper di port default untuk connect ke kafka di remote server, masukkan parameter saat buat client di server.js

--- server.js
+++ server.js
@@ -20,7 +20,30 @@
 
 	// Primary Variable
 	const App = Express();
+	var kafka = require("kafka-node"),
+	Producer = kafka.Producer,
+	client = new kafka.KafkaClient(),
+	producer = new Producer(client);
 
+	let count = 0;
+
+	producer.on("ready", function() {
+		console.log("ready");
+		setInterval(function() {
+			payloads = [
+				{ topic: "cat", messages: `I have ${count} cats`, partition: 0 }
+			];
+
+			producer.send(payloads, function(err, data) {
+				console.log(data);
+				count += 1;
+			});
+		}, 5000);
+	});
+
+	producer.on("error", function(err) {
+		console.log(err);
+	});
 /*
 |--------------------------------------------------------------------------
 | APP Init
