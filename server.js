eb8771fb62e7934d9b2e86d4929fa0cd3c04f70c
nicholas <silvranz@gmail.com>
2019-08-19 15:20:26 +0700

    contoh spark

--- server.js
+++ server.js
@@ -20,6 +20,10 @@
 
 	// Primary Variable
 	const App = Express();
+	var data_source_request = {
+		"auth":false,
+		"finding":false
+	}
 	var kafka = require("kafka-node"),
 	Producer = kafka.Producer,
 	Consumer = kafka.Consumer,
@@ -28,14 +32,92 @@
 	consumer = new Consumer(
         client,
         [
-            { topic: 'kafkaRequestData', partition: 0 },{ topic: 'kafkaResponse', partition: 0 }
+            { topic: 'kafkaRequestData', partition: 0 },{ topic: 'kafkaDataCollectionProgress', partition: 0 },{ topic: 'kafkaResponse', partition: 0 }
         ],
         {
             autoCommit: false
         }
     );
 	consumer.on('message', function (message) {
-		console.log(message,JSON.parse(message.value));
+		json_message = JSON.parse(message.value);
+		if(message.topic=="kafkaRequestData"){
+			//ada yang request data ke microservices
+			let reqDataObj;
+			let responseData = false;
+			if(json_message.msa_name=="finding"){
+				reqDataObj = {
+					"msa_name":"finding",
+					"requester":"finding",
+					"request_id":1,
+					"data":[
+						{
+							"FINDING_CODE":"123",
+							"CREATOR":"01"
+						},
+						{
+							"FINDING_CODE":"124",
+							"CREATOR":"02"
+						},
+					]
+				}
+				responseData = true;
+			}
+			else if(json_message.msa_name=="auth"){
+				reqDataObj = {
+					"msa_name":"auth",
+					"requester":"finding",
+					"request_id":1,
+					"data":[
+						{
+							"CREATOR":"01",
+							"NAME":"FER"
+						},
+						{
+							"CREATOR":"02",
+							"NAME":"FER2"
+						},
+					]
+				}
+				responseData = true;
+			}
+			if(responseData){
+				//lempar data microservices ke spark
+				let payloads = [
+					{ topic: "kafkaResponseData", messages: JSON.stringify(reqDataObj), partition: 0 }
+				];
+				/*producer.send( payloads, function( err, data ) {
+					console.log( "Send data to kafka",data );
+				});*/
+			}
+		}
+		else if(message.topic=="kafkaDataCollectionProgress"){
+			//update progress data collection
+			let data_complete = true;
+			data_source_request[json_message.msa_name] = true;
+			for(let x in data_source_request){
+				if(data_source_request[x]==false){
+					data_complete = false;
+				}
+			}
+			/*if(data_complete){
+				var SSH = require('simple-ssh');
+
+				var ssh = new SSH({
+					host: '149.129.252.13',
+					user: 'root',
+					pass: 'T4pagri123'
+				});
+
+				ssh.exec('/root/spark/bin/spark-submit /root/pyspark/code/collecting.py requester=finding/request_id=1 > /root/pyspark/output/log.txt', {
+					out: function(stdout) {
+						console.log(stdout);
+					}
+				}).start();
+			}*/
+		}
+		else if(message.topic=="kafkaResponse"){
+			console.log(message,json_message);
+		}
 	});
 
 	let count = 0;
@@ -49,30 +131,21 @@
 		}],
 		"query":"SELECT * FROM auth a JOIN finding f ON f.INSERT_USER=a.USER_ID",
 		"requester":"finding",
-		"request_id":0
+		"request_id":1
 	}
-	let reqDataObj = {
-		"queue_id":0,
-		"msa_name":"finding",
-		"model_name":"FindingModel",
-		"agg":{}
-	}
-	//{ topic: "kafkaResponseData", messages: JSON.stringify(reqDataObj), partition: 0 }
 
 	producer.on("ready", function() {
-		console.log("ready",reqObj.toString());
-		setInterval(function() {
+		//console.log(JSON.stringify(reqObj));
+		//setInterval(function() {
+			//minta data
 			payloads = [
-				{ topic: "kafkaRequest", messages: JSON.stringify(reqDataObj), partition: 0 }
+				{ topic: "kafkaRequest", messages: JSON.stringify(reqObj), partition: 0 }
 			];
 
-			producer.send( payloads, function( err, data ) {
-				console.log( "Send to kafka" );
-				// console.log( "HEHEHEHEHE" );
-				// console.log( data );
-				// count += 1;
-			});
-		}, 2000);
+			/*producer.send( payloads, function( err, data ) {
+				console.log( "Send to kafka request data" );
+			});*/
+		//}, 2000);
 	});
 
 	producer.on("error", function(err) {
--- test.js
+++ test.js
@@ -1,10 +1,27 @@
 var kafka = require("kafka-node"),
-  Consumer = kafka.Consumer,
-  client = new kafka.KafkaClient(),
-  consumer = new Consumer(client, [{ topic: "cat", partition: 0 }], {
+	Producer = kafka.Producer,
+	Consumer = kafka.Consumer,
+	client = new kafka.KafkaClient({kafkaHost : "149.129.252.13:9092"}),
+	producer = new Producer(client),    
+  consumer = new Consumer(client, [{ topic: "kafkaResponse", partition: 0 }], {
     autoCommit: false
   });
 
+let reqDataObj = {
+	"msa_name":"auth",
+	"requester":"finding",
+	"request_id":1,
+	"data":[
+		{
+			"CREATOR":"01",
+			"NAME":"FER"
+		},
+		{
+			"CREATOR":"02",
+			"NAME":"FER"
+		},
+	]
+}
 consumer.on("message", function(message) {
   console.log(message);
   
